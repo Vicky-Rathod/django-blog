f237fc3fb1ce99abbc7ec967ae92e31bd1b0ac82
mohammadanarul <mdanarul7075@gmail.com>
2021-07-13 06:21:46 +0600

    Added social_links and skills app.

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
+from blog.modes import Post
 
 # Create your models here.
+class Like(models.Model):
+    pass
+
+class Dislike(models.Model):
+    pass
