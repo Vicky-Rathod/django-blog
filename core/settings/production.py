dd803563c0d7493f7969cbef4a664b5c5c7bd255
mohammadanarul <mdanarul7075@gmail.com>
2021-07-30 12:17:35 +0600

    update production py.

--- core/settings/production.py
+++ core/settings/production.py
@@ -32,17 +32,28 @@ django_heroku.settings(locals())
 # whitenoise collectstatic
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_BACKEND = config('EMAIL_BACKEND')
+# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mdanarul7075@gmail.com'
+EMAIL_HOST_PASSWORD = 'ydaqqzkuvbwovopf'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'mdanarul7075@gmail.com'
 
 cloudinary.config( 
-  cloud_name = config('cloud_name'),
-  api_key = config('api_key'),
-  api_secret = config('api_secret'),
+  cloud_name = 'mohammadanarul',
+  api_key = '867477367854119',
+  api_secret = 'Q4Wx4-s3DS9Zxf57tHk3uDX3WfY',
 )
+# cloudinary.config( 
+#   cloud_name = config('cloud_name'),
+#   api_key = config('api_key'),
+#   api_secret = config('api_secret'),
+# )
